657551c8e39616ca333530bb7f0761c8ee192568
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-29 16:09:44 -0500

    added missing imports

--- src/Decks/Deck.js
+++ src/Decks/Deck.js
@@ -1,3 +1,4 @@
+import React from "react";
 import DeckOptions from "./DeckOptions";
 
 function Deck() {
--- src/Decks/Decks.js
+++ src/Decks/Decks.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Deck from "./Deck";
 
 function Decks() {
