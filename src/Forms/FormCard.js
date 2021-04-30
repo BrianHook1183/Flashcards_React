c411aa489f0fc4c3de937734631feff7b5c180b7
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-30 17:12:03 -0500

    breadcrumb path

--- src/Forms/FormCard.js
+++ src/Forms/FormCard.js
@@ -1,15 +1,33 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function FormCard() {
   const { cardId, deckId } = useParams();
   const placeHolder = (
     <p>
-      Edit Card with id {cardId} for deck {deckId}
+      Edit Card with id {cardId} in deck {deckId}
     </p>
   );
+  //TODO replace with actual name of deck in breadcrumb path
 
-  return placeHolder;
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li class="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>Name of Deck {deckId}</Link>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Edit Card {cardId}
+          </li>
+        </ol>
+      </nav>
+      {placeHolder}
+    </div>
+  );
 }
 
 export default FormCard;
--- src/Forms/FormDeck.js
+++ src/Forms/FormDeck.js
@@ -1,11 +1,29 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function FormDeck() {
   const { deckId } = useParams();
   const placeHolder = <p>Edit Deck with id {deckId}</p>;
+  //TODO replace with actual name of deck in breadcrumb path
 
-  return placeHolder;
+  return (
+    <div>
+      <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li class="breadcrumb-item">
+            <Link to={`/decks/${deckId}`}>Name of Deck {deckId}</Link>
+          </li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Edit Deck
+          </li>
+        </ol>
+      </nav>
+      {placeHolder}
+    </div>
+  );
 }
 
 export default FormDeck;
